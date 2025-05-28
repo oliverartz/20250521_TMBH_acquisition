a80439b2e08e1eec7c5f6e2d49e2d1fc563cf288
Oliver Artz <artz.oliver@gmail.com>
2025-05-28 09:58:09 -0400

    Add sync script for MAF files to HPC for processing

--- analysis/hpc/sync_to_hpc.sh
+++ analysis/hpc/sync_to_hpc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Manually sync intermediate files to HPC for further processing
+hpc_dir="/data/ldiaz/artzo/analysis/20250521_TMBH_acquisition"
 
 # MAF files
+maf_dir="/Users/artzo/Documents/4_projects/20241016_TKI_TMB_increase/20250521_TMBH_acquisition/data/processed/006_maf"
+
+rsync -avzPh ${maf_dir}/* artzo@lilac:${hpc_dir}/data/processed/006_maf
