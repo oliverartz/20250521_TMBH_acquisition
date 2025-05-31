c3949fd637589c2925faeafc3c06c086628b97eb
Oliver Artz <artz.oliver@gmail.com>
2025-05-31 11:43:57 -0400

    Add synchronization for netMHCpan results and additional patient data files

--- analysis/hpc/sync_from_hpc.sh
+++ analysis/hpc/sync_from_hpc.sh
@@ -8,3 +8,9 @@ vcf_dir="/data/ldiaz/artzo/analysis/20250521_TMBH_acquisition/data/processed/hpc
 
 mkdir -p ${local_dir}/data/processed/hpc/vcf
 rsync -avzPh artzo@lilac:${vcf_dir}/* ${local_dir}/data/processed/hpc/vcf/
+
+# Summarized netMHCpan results
+netmhcpan_results="/data/ldiaz/artzo/analysis/20250521_TMBH_acquisition/data/processed/hpc/006_patient_status_compared_to_baseline_phbr_vaf.txt"
+
+mkdir -p ${local_dir}/data/processed/hpc
+rsync -avzPh artzo@lilac:${netmhcpan_results} ${local_dir}/data/processed/hpc/
--- analysis/hpc/sync_to_hpc.sh
+++ analysis/hpc/sync_to_hpc.sh
@@ -7,3 +7,14 @@ hpc_dir="/data/ldiaz/artzo/analysis/20250521_TMBH_acquisition"
 maf_dir="/Users/artzo/Documents/4_projects/20241016_TKI_TMB_increase/20250521_TMBH_acquisition/data/processed/006_maf"
 
 rsync -avzPh ${maf_dir}/* artzo@lilac:${hpc_dir}/data/processed/006_maf
+
+# patient key
+patient_key_dir="/Users/artzo/Documents/4_projects/20241016_TKI_TMB_increase/20250521_TMBH_acquisition/data/processed/001_patient_key_mod.txt"
+
+rsync -avzPh ${patient_key_dir} artzo@lilac:${hpc_dir}/data/processed/001_patient_key_mod.txt
+
+# annotated mutations (progression samples)
+
+mutations_file="/Users/artzo/Documents/4_projects/20241016_TKI_TMB_increase/20250521_TMBH_acquisition/data/processed/005_annotated_mutations_progression.txt"
+
+rsync -avzPh ${mutations_file} artzo@lilac:${hpc_dir}/data/processed/005_annotated_mutations_progression.txt
